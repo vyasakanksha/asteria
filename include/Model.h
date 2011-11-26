4757a3272c6cde0bc531ab9c38b93b5dde0cbe2e
Samuel C. Payson <scpayson@gmail.com>
2011-11-26 18:02:21 -0500

    Created preliminary design for the 'Model' class.
    This class will represent general 3D models.

---
+++ include/Model.h
@@ -0,0 +1,49 @@
+/****************************************************************************
+ *                                                                          *
+ * This file is part of the Asteria project.                                *
+ * Copyright (C) 2011 Samuel C. Payson, Akanksha Vyas                       *
+ *                                                                          *
+ * Asteria is free software: you can redistribute it and/or modify it under *
+ * the terms of the GNU General Public License as published by the Free     *
+ * Software Foundation, either version 3 of the License, or (at your        *
+ * option) any later version.                                               *
+ *                                                                          *
+ * Asteria is distributed in the hope that it will be useful, but WITHOUT   *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or    *
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License    *
+ * for more details.                                                        *
+ *                                                                          *
+ * You should have received a copy of the GNU General Public License along  *
+ * with Asteria. If not, see <http://www.gnu.org/licenses/>.                *
+ *                                                                          *
+ ****************************************************************************/
+
+#ifndef _ASTERIA_MODEL_H_
+#define _ASTERIA_MODEL_H_
+
+#include "altio.h"
+#include "libInclude.h"
+
+namespace asteria {
+
+   class Model {
+      public:
+         virtual ~Model() = 0;
+
+         // The code that calls these functions is responsible for performing
+         // any transformations that are necessary for the model to render
+         // correctly.
+
+         // 'anim' is the number of the animation sequence being rendered.
+         // 'time' is the number of milliseconds since the animation started.
+         virtual bool DrawFrame( int anim, int time ) = 0;
+
+         virtual bool DrawStatic( void ) = 0;
+
+      private:
+
+   };
+
+};
+
+#endif // Model.h
